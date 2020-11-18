78eca8951470a268cbc4d9786c009c830aee379d
zzb610 <bwyuchi@163.com>
2020-11-18 21:14:47 +0800

    update

--- num_sequence/fibonacci.cc
+++ num_sequence/fibonacci.cc
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include "fibonacci.h"
 #include "num_sequence.h"
 
@@ -57,33 +56,3 @@ void Fibonacci::GenElems(int pos) const
         }
     }
 }
-=======
-#include "fibonacci.h"
-#include "num_sequence.h"
-
-#include <vector>
-using std::vector;
-
-vector<int> Fibonacci::elems_;
-MEIvoid Fibonacci::GenElems(int pos) const
-{
-    if (elems_.empty())
-    {
-        elems_.push_back(1);
-        elems_.push_back(1);
-    }
-
-    if (elems_.size() <= pos)
-    {
-        int i = elems_.size();
-        int n_2 = elems_[i - 2], n_1 = elems_[i - 1];
-        for (; i < pos + 1; ++i)
-        {
-            int elem = n_2 + n_1;
-            elems_.push_back(elem);
-            n_2 = n_1;
-            n_1 = elem;
-        }
-    }
-}
->>>>>>> 42dd1595a04f4dc91aa43058a60f34cb19240577
--- num_sequence/num_sequence.cc
+++ num_sequence/num_sequence.cc
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:num_sequence/num_sequece.cc
 #include "num_sequence.h"
 
 #include <iostream>
@@ -28,45 +27,3 @@ ostream &operator<<(ostream &os, const NumSequence &ns)
 {
     return ns.print(os);
 }
-=======
-#include "num_sequence.h"
-
-#include <iostream>
-
-using std::cerr;
-
-const int NumSequence::kMaxElems = 1024;
-
-// dtor
-NumSequence::~NumSequence() {}
-
-int NumSequence::Elem(int pos)
-{
-    if(!CheckIntegrity(pos, relems_.size()))
-    {
-        return 0;
-    }
-
-    return relems_[];
-}
-
-bool NumSequence::CheckIntegrity(int pos, int size)
-{
-    if (pos <= 0 || pos > kMaxElems)
-    {
-        cerr << "!! invaild position: " << pos << " Cannot honor request\n";
-        return false;
-    }
-    if (pos > size)
-    {
-        GenElems(pos);
-    }
-    return true;
-}
-
-ostream &operator<<(ostream &os, const NumSequence &ns)
-{
-    return ns.print(os);
-}
-
->>>>>>> 42dd1595a04f4dc91aa43058a60f34cb19240577:num_sequence/num_sequence.cc
