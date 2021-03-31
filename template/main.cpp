4e30880412ae43c3e5f9b3b0f3539aff49dbc55f
zzb610 <bwyuchi@163.com>
2021-03-31 21:01:59 +0800

    template

--- template/main.cpp
+++ template/main.cpp
@@ -25,27 +25,127 @@ struct pair
     T1 fisrt;
     T2 second;
 
-    pair() : fisrt(T1()), second(T2()){}
-    pair(const T1& a, const T2&b): fisrt(a), second(n){}
+    pair() : fisrt(T1()), second(T2()) {}
+    pair(const T1 &a, const T2 &b) : fisrt(a), second(b) {}
 
-    template<class U1, class U2>
-    pair(const pair<U1, U2>& p): fisrt(p.fisrt), second(p.second){}
+    template <class U1, class U2>
+    pair(const pair<U1, U2> &p) : fisrt(p.fisrt), second(p.second) {}
 };
 
-// shared_ptr
-template<typename _Tp>
-class shared_ptr
+// // shared_ptr
+// template <typename _Tp>
+// class shared_ptr
+// {
+//     template <typename _Tp1>
+//     explicit shared_ptr(_Tp1 *__p) : __shared_ptr<_Tp>(__p) {}
+// };
+
+// template specialization
+namespace zzb
 {
-    template<typename _Tp1>
-    explicit shared_ptr(_Tp1* __p): __shared_ptr<_Tp>(__p){}
+    template <typename T>
+    struct hash
+    {
+    };
+
+    template <>
+    struct hash<char>
+    {
+        size_t operator()(char x) const
+        {
+            return x;
+        }
+    };
+
+    template <>
+    struct hash<int>
+    {
+        size_t operator()(int x) const
+        {
+            return x;
+        }
+    };
+}
+
+// partial specialization
+
+// number partial
+namespace zzb
+{
+    template <typename T, typename Alloc>
+    class vector
+    {
+    };
+
+    template <typename Alloc>
+    class vector<int, Alloc>
+    {
+    };
+}
+// range partial
+template <typename T>
+class C
+{
 };
+
+template <typename T>
+class C<T *>
+{
+};
+
+// template template parameter
+namespace zzb
+{
+    // template <typename T, template <typename T> class Container>
+    // class XCls
+    // {
+    // private:
+    //     Container c;
+    // };
+
+    template <typename T, template <typename U> class SmartPtr>
+    class XCls
+    {
+    public:
+        XCls() : sp(new T) {}
+
+    private:
+        SmartPtr<T> sp;
+    };
+}
+
+#include <deque>
+// this is NOT template template parameter
+namespace zzb
+{
+    using std::deque;
+    template<typename T, class Sequece=deque<T>>
+    class stack
+    {
+        friend bool operator==(const stack&, const stack&);
+        friend bool operator<(const stack&, const stack&);
+
+        protected:
+            Sequece c;
+    };
+}
+
 
 class Base1;
 class Derived1;
+using namespace std;
+#include <string>
+#include <memory>
+#include <list>
 int main(int, char **)
 {
 
-    Base1* ptr = new Derived1; // up-cast
-    shared_ptr<Base1> sptr(new Derived1); // up-cast like
-    std::cout << "Hello, world!\n";
+    // Base1* ptr = new Derived1; // up-cast
+    // shared_ptr<Base1> sptr(new Derived1); // up-cast like
+    // std::cout << "Hello, world!\n";
+
+    // cout << zzb::hash<int>()(1000) << endl;
+    // zzb::XCls<string, shared_ptr> p1;
+    zzb::stack<int> s1;
+    zzb::stack<int, list<int>> s2;
 }
